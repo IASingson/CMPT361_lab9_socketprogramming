07e602aa49b74f8c2a63fcaeff556112079464d4
IASingson <singsoni@mymacewan.ca>
2026-03-18 23:29:46 -0600

    connection works, terminating connection works,  main loop works, updating the dictionary per instructions works

---
+++ Client.py
@@ -0,0 +1,79 @@
+# Ingrid Andre Singson
+# 3146981
+
+# Using some code from assignment 1 to help create the program for this lab
+import socket 
+import sys
+
+FORMAT = 'ascii'
+SIZE = 2048
+
+def client():
+    # server information
+    server_name = 'localhost'
+    server_port = 13000
+
+    # create client socket using IPv4 and TCP protocols
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error in client socket creation:', e)
+        sys.exit(1)
+
+    try: 
+        # client connects with the server
+        client_socket.connect((server_name, server_port))
+
+        # welcome message from the server
+        msg_from_server = client_socket.recv(SIZE).decode(FORMAT)
+
+        # print msg from server to client then send client choice to server
+        msg_to_server = input(msg_from_server)
+        client_socket.send(msg_to_server.encode(FORMAT))
+
+        # keep running program as long as client does not choose 3
+    
+        while msg_to_server != '3':
+
+            msg_from_server = client_socket.recv(SIZE).decode(FORMAT)
+            
+            if msg_to_server == '1':
+                
+                # send server the client name
+                msg_to_server = input(msg_from_server).encode(FORMAT)
+                client_socket.send(msg_to_server)
+
+                # send server client phone number
+                msg_from_server = client_socket.recv(SIZE).decode(FORMAT)
+                msg_to_server = input(msg_from_server).encode(FORMAT)
+                client_socket.send(msg_to_server)
+
+                # get server menu options again
+                msg_from_server = client_socket.recv(SIZE).decode(FORMAT)
+
+                # send client choice to server
+                msg_to_server = input(msg_from_server)
+                client_socket.send(msg_to_server.encode(FORMAT))
+            
+            if msg_to_server == '2':
+
+                # get server menu options again
+                msg_from_server = client_socket.recv(SIZE).decode(FORMAT)
+
+                # send client choice to server
+                msg_to_server = input(msg_from_server)
+                client_socket.send(msg_to_server.encode(FORMAT))
+
+        
+
+        client_socket.close()
+        print('Connection terminated.')
+
+
+    except socket.error as e:
+        print('An error occurred:', e)
+        client_socket.close()
+        sys.exit(1)
+
+#-----------------------------------------------------
+client()
---
+++ Server.py
@@ -0,0 +1,109 @@
+# Ingrid Andre Singson 
+# 3146981
+
+# Using some code from assignment 1 to help create program for this lab 
+import socket
+import sys
+
+FORMAT = 'ascii'
+SIZE = 2048
+
+def server():
+    # server port
+    server_port = 13000
+
+    # creating a server socket that uses IPv4 and TCP protocols
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print('Error in server socket creation:', e)
+        sys.exit(1)
+
+    # associate 13000 port number to the server socket
+    try:
+        server_socket.bind(('', server_port))
+    except socket.error as e:
+        print('Error in server socket binding:', e)
+        sys.exit(1)
+
+    # only one client allowed at each time 
+    server_socket.listen(1)
+
+    while 1:
+        try:
+            # server accepts client connection 
+            connection_socket, addr = server_socket.accept()
+
+            # send client welcome message and server menu options
+            msg_to_client = 'Welcome to the online phone book.\n\nPlease select the operation\n1)' \
+            'Add a new entry\n2)Search\n3)Terminate the connection\n\nChoice: '.encode(FORMAT)
+
+            connection_socket.send(msg_to_client)
+
+            # server receives client menu option 
+            msg_from_client = connection_socket.recv(SIZE).decode(FORMAT)
+
+            phonebook = {}
+
+            while msg_from_client != '3':
+
+                server_menu = '\nPlease select the operation \n1)Add a new entry\n2_Search\n3|Terminate the connection\n\nChoice: '.encode(FORMAT)
+
+                # perform phone adding subprotocol
+                if msg_from_client == '1':
+
+                    # ask client for name
+                    msg_to_client = 'Enter the name: '.encode(FORMAT)
+                    connection_socket.send(msg_to_client)
+                    
+                    name = connection_socket.recv(SIZE).decode(FORMAT)
+
+                    # ask client for phone number 
+                    msg_to_client = 'Enter the phone number: '.encode(FORMAT)
+                    connection_socket.send(msg_to_client) 
+
+                    phone_number = connection_socket.recv(SIZE).decode(FORMAT)
+
+                    # add information to phonebook
+                    # if name already exists, add number under name
+                    if name in phonebook:
+                        phonebook[name].append(phone_number)
+                    
+                    # if name does NOT exist, create a new entry 
+                    else:
+                        phonebook[name] = [phone_number]
+                    
+                    # send client server menu options 
+                    connection_socket.send(server_menu)
+
+                    # receive client menu option
+                    msg_from_client = connection_socket.recv(SIZE).decode(FORMAT)
+
+         
+
+                if msg_from_client == '2':
+
+                    # send client server menu options
+                    connection_socket.send(msg_to_client)
+
+                    # receive client menu option
+                    msg_from_client = connection_socket.recv(SIZE).decode(FORMAT)
+
+                # Phone search subprotocol
+            
+            
+    
+            connection_socket.close()
+
+        except socket.error as e:
+            print('An error occurred', e)
+            server_socket.close()
+            sys.exit(1)
+        except:
+            print('Goodbye')
+            server_socket.close()
+            sys.exit(0)
+
+#-----------------------------------------------------
+
+server()
